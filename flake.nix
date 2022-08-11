f1804d90a1d96d488addb991b22bddbaa73d8abd
lucasew <lucas59356@gmail.com>
2022-08-11 15:49:12 -0300

    mais coisa de flake
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- flake.nix
+++ flake.nix
@@ -1,8 +1,31 @@
 {
   description = "UTFOS";
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-22.06";
+    nixpkgs.url = "nixpkgs/nixos-22.05";
   };
-  outputs = { self, nixpkgs, ... }: {
+  outputs = { self, nixpkgs, ... }@args:
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+      config = {
+        allowUnfree = true;
+      };
+    };
+    inherit (pkgs) lib;
+    inherit (lib) concatStringsSep attrValues mapAttrs;
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      name = "devshell";
+      shellHook = ''
+        PATH="$(pwd)/bin:$PATH"
+        NIX_PATH="${concatStringsSep ":" (
+          attrValues (
+            mapAttrs (k: v: "${k}=${v}") args
+          )
+        )}:nixpkgs-overlays=$(pwd)/compat/overlay.nix"
+      '';
+    };
+    inherit pkgs; # pra poder usar no nix-repl fácil só com :lf
   };
 }
