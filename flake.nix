3cb8f1af0ef7345cba05aee57c4e295ec9c87849
lucasew <lucas59356@gmail.com>
2022-08-25 20:34:55 -0300

    poc-ubuntu -> poc_ubuntu
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- flake.nix
+++ flake.nix
@@ -37,7 +37,7 @@
       nodes = names: listToAttrs (map (name: { inherit name; value = node name; }) names);
     in nodes [
       "demo"
-      "poc-ubuntu"
+      "poc_ubuntu"
     ];
     inherit pkgs lib; # pra poder usar no nix-repl fácil só com :lf
     overlays = {
