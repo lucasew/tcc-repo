03b4382cd501b8bdb212cce7699a7971730bc4fc
lucasew <lucas59356@gmail.com>
2022-08-25 22:22:04 -0300

    add gnome
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- nodes/poc_ubuntu/default.nix
+++ nodes/poc_ubuntu/default.nix
@@ -1,4 +1,4 @@
-{...}:
+{pkgs, ...}:
 {
   imports = [
     ./hardware-configuration.nix
@@ -6,4 +6,22 @@
   ];
   utfos.machine-group = [ "poc" ];
   boot.loader.grub.devices = [ "/dev/sda" ];
+
+  # gui
+  services = {
+    xserver = {
+      enable = true;
+      desktopManager = {
+        xterm.enable = true;
+        gnome.enable = true;
+      };
+      displayManager.gdm.enable = true;
+    };
+  };
+  environment.systemPackages = with pkgs.gnomeExtensions; [
+    night-theme-switcher
+    sound-output-device-chooser
+    gsconnect
+    transparent-shell
+  ];
 }
