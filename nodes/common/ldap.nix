890d48c18bccd8e7b0671c64a62c7529f2578e84
lucasew <lucas59356@gmail.com>
2022-09-29 22:48:49 -0300

    tentando com o SSSD
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- nodes/common/ldap.nix
+++ nodes/common/ldap.nix
@@ -2,23 +2,50 @@
 # credits for
 # - https://mt-caret.github.io/blog/posts/2020-07-25-ldap-client-with-nixos.html
 # - https://www.forumsys.com/2022/05/10/online-ldap-test-server/
+# - https://docs.oracle.com/cd/E37670_01/E41138/html/ol_sssd_ldap.html
 {
-  users.ldap = {
-    enable = true;
-    daemon.enable = true;
-    base = "dc=example,dc=com";
-    bind.policy = "soft";
-    bind.distinguishedName = "cn=admin,dc=example,dc=com";
-    # users: riemann gauss euler euclid
-    bind.passwordFile = builtins.toFile "passwd-file" "password";
-    # server = "ldap://192.168.100.50";
-    server = "ldap://192.168.100.50";
-    # useTLS = true;
-    loginPam = true;
-    extraConfig = ''
-      ldap_version 3
-    '';
-  };
+  services.sssd.enable = true;
+  services.sssd.config = ''
+    [sssd]
+    config_file_version 2
+    domains = default
+    services = nss, pam
+
+    [domain/default]
+    id_provider = ldap
+    access_provider = ldap
+    auth_provider = ldap
+    ldap_uri = ldap://192.168.100.50
+    ldap_id_use_start_tls = false
+    ldap_search_base = dc=example,dc=com
+
+    [nss]
+    reconnection_retries = 3
+    entry_cache_timeout = 5
+
+    [pam]
+    reconnection_retries = 3
+    offline_credentials_expiration = 2
+    offline_failed_login_attempts = 3
+    offline_failed_login_delay = 5
+
+  '';
+  # users.ldap = {
+  #   enable = true;
+  #   daemon.enable = true;
+  #   base = "dc=example,dc=com";
+  #   bind.policy = "soft";
+  #   bind.distinguishedName = "cn=admin,dc=example,dc=com";
+  #   # users: riemann gauss euler euclid
+  #   bind.passwordFile = builtins.toFile "passwd-file" "password";
+  #   # server = "ldap://192.168.100.50";
+  #   server = "ldap://192.168.100.50";
+  #   # useTLS = true;
+  #   loginPam = true;
+  #   extraConfig = ''
+  #     ldap_version 3
+  #   '';
+  # };
   # security.pam.services.sshd = {
   #   makeHomeDir = true;
   #   text = lib.mkDefault (
