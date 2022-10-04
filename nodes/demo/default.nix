a05ced5767943bb0750cc24fa5b539b68e716bf0
lucasew <lucas59356@gmail.com>
2022-10-04 18:17:16 -0300

    primeira iteração do instalador automágico
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- nodes/demo/default.nix
+++ nodes/demo/default.nix
@@ -3,9 +3,4 @@
     ../common
   ];
 
-  boot.loader.grub.devices = [ "/dev/sda" "/dev/nvme0n1" ];
-  fileSystems."/" = {
-    label = "nixos";
-    fsType = "ext4";
-  };
 }
--- nodes/installer/default.nix
+++ nodes/installer/default.nix
@@ -1,9 +1,26 @@
 {pkgs, modulesPath, self, lib, ...}: {
   imports = [
     "${modulesPath}/installer/cd-dvd/installation-cd-minimal.nix"
+    ../bootstrap
   ];
 
   networking.hostName = "install";
 
-  environment.etc."_nixos-bootstrap-generation".source = self.outputs.nixosConfigurations.demo.config.system.build.toplevel;
+  environment.etc."_nixos-bootstrap-generation".source = (self.outputs.nixosConfigurations.demo.extendModules {
+    modules = [
+      ({...}: {
+        boot.loader.grub.devices = [ "/dev/sda" "/dev/nvme0n1" ];
+        fileSystems."/" = {
+          label = "nixos";
+          fsType = "ext4";
+        };
+      })
+    ];
+  }).config.system.build.toplevel;
+  environment.etc."code".source = ../../.;
+
+  # defaults set up by the installer module that we don't want
+  networking.wireless.enable = false;
+
+  system.stateVersion = lib.mkForce "22.05";
 }
