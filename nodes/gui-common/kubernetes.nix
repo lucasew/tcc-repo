8bc3ae80040959de745caa7e960e845aa593c16d
lucasew <lucas59356@gmail.com>
2022-10-07 13:44:27 -0300

    liberar 80 e 443 TCP no firewall
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- nodes/gui-common/kubernetes.nix
+++ nodes/gui-common/kubernetes.nix
@@ -5,4 +5,5 @@
     serverAddr = lib.mkIf (config.services.k3s.role == "agent") "https://192.168.0.101:6443";
     tokenFile = lib.mkIf (config.services.k3s.role == "agent") "/var/lib/rancher/k3s/agent-token";
   };
+  networking.firewall.allowedTCPPorts = [ 80 443 ];
 }
